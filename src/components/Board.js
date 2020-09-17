9694eb662623003d491c1679351f7d7d47f51d92
tfunk2 <tyler.j.funk2@gmail.com>
2020-09-17 05:16:13 -0600

    fixed issue with boxIndex prop not assigning properly

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,11 @@ import '../stylesheets/Board.css'
 import Square from './Square.js'
 
 export default function Board(props){
+
+    let indexCounter = -1;
     const makeNineSquares = props.game.map(square => {
-        return <Square boxIndex={props.game.indexOf(square)}
+        indexCounter++
+        return <Square boxIndex={indexCounter}
                 checkBoard={props.checkBoard} 
                 isXTurn={props.isXTurn} 
                 updateGame={props.updateGame} 

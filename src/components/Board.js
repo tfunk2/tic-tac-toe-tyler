098bbd922fe6b84d0808fe127501f7a957cf2340
tfunk2 <tyler.j.funk2@gmail.com>
2020-09-18 00:15:13 -0600

    cleaned up Board.js formatting

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,15 +5,15 @@ import Square from './Square.js'
 export default function Board(props){
 
     let indexCounter = -1;
-    const makeNineSquares = props.game.map(square => {
+    const makeNineSquares = props.game.map(() => {
         indexCounter++
         return <Square boxIndex={indexCounter}
-                checkBoard={props.checkBoard} 
-                isXTurn={props.isXTurn} 
-                updateGame={props.updateGame} 
-                game={props.game}
-                checkForWin={props.checkForWin}
-            />
+            checkBoard={props.checkBoard} 
+            isXTurn={props.isXTurn} 
+            updateGame={props.updateGame} 
+            game={props.game}
+            checkForWin={props.checkForWin}
+        />
     })
 
     return (
